9dae56bb136eb226f756c466f45783861b7a48c6
shantunu007 <chakmashantunu.web@gmail.com>
2026-02-06 22:30:03 +0600

    added email verification using Nodemailer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer"
 
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    auth: {
+        user: process.env.APP_USER,
+        pass: process.env.APP_PASS,
+    },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -34,7 +45,20 @@ export const auth = betterAuth({
     },
     emailVerification: {
         sendVerificationEmail: async ({ user, url, token }, request) => {
-            console.log("Verification email sent");
+            try {
+                const info = await transporter.sendMail({
+                    from: '"Prisma Blog" <chakmashantunu.web@gmail.com>',
+                    to: "sunghajung0192581@gmail.com",
+                    subject: "Hello ✔",
+                    text: "Hello world?",
+                    html: "<b>Hello world?</b>",
+                });
+
+                console.log("Email sent:", info.messageId);
+            } catch (error) {
+                console.error("Email send failed:", error);
+            }
+
         },
     }
 });
