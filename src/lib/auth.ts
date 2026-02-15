09cc56667e292469595afe574811c36faf410f59
shantunu007 <chakmashantunu.web@gmail.com>
2026-02-15 21:44:38 +0600

    enhance Google OAuth config and enable auto sign-in after email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,6 +45,7 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendOnSignUp: true,
+    autoSignInAfterVerification: true,
     sendVerificationEmail: async ({ user, url, token }, request) => {
       try {
         console.log({ user, url, token })
@@ -134,6 +135,8 @@ export const auth = betterAuth({
 
   socialProviders: {
     google: {
+      prompt: "select_account consent",
+      accessType: "offline",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
